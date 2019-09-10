66a15995ca81d163e0ebdb33668f5a3d6a549d4a
Seth Broder <seth.broder@gmail.com>
2019-09-10 11:56:34 -0400

    Return encrypted to form

--- main.py
+++ main.py
@@ -9,26 +9,26 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method="post">
             <label for="Rotate by:">Rotate by:</label>
             <input type="text" name="rot" value="0">
-            <textarea name="text"></textarea>
+            <textarea name="text">{0}</textarea>
             <input type="submit" value="Submit Query">
         </form>
     </body>
@@ -40,12 +40,12 @@ def encrypt():
     rotate = int(request.form['rot'])
     words = request.form['text']
     encrypted = rotate_string(words, rotate)
-    content = "<h1>" + encrypted + "</h1>" 
-    return content
+    #content = "<h1>" + encrypted + "</h1>" 
+    return form.format(encrypted)
 
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 app.run()
